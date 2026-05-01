3f19fc42192067c2edb139790c0203f7f8aadd77
opljkl802-bit <opljkl802@gmail.com>
2026-05-01 08:24:50 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(os.environ.get("API_ID", "30296254"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8543000414:AAG4h4r0HtAZUBH6tnTR0ACM5DjzT33b0hU")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8683790804:AAGbrDOJgqg5F5McaIXYHbdOuQECVdAdytY")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("Masterextratorraonebot")
 # ------------------X------------------------------
